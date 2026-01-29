9bd4f946d58d9a579ba108c20656e97ba1ffc1bf
Alexius Osemeke <alexiusosemeke@gmail.com>
2026-01-29 11:51:31 +0100

    contains my python tutorial codes and exercises

--- functions.py
+++ functions.py
@@ -20,4 +20,38 @@ def showNumber(number):
     for i in range(1, number + 1):
         print(i)
 
-showNumber(5)
+# showNumber(5)
+
+# MINI PROJECT
+# PRINT NUMBER FUNCTION
+
+# def printNumber(number):
+#     for i in range(1, number + 1):
+#         return i
+
+# Function that checks if a number is odd or even
+
+def checkEvenOdd(number):
+    if number % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+
+result = checkEvenOdd(51)
+
+print(result)
+
+# FIND THE SUM OF NUMBERS USING WHILE LOOP
+
+def calculateSum(n):
+    total = 0
+    i = 1
+
+    while i <= n:
+        total += i
+        i += 1
+
+    return total
+
+result = calculateSum(10)
+print(result)
