a0661667574f9781fb8d9680c61057dc6aa35111
Alexius Osemeke <alexiusosemeke@gmail.com>
2026-01-29 11:33:54 +0100

    contains my python tutorial codes and exercises

--- functions.py
+++ functions.py
@@ -8,3 +8,16 @@
 # - to make a program easier to read and maintain
 
 # How to declare a function in python:
+
+# we use the def keyword to define a function. An example below:
+
+def greet():
+    print('Hello')
+
+# The function above will automatically greet the user when called.
+
+def showNumber(number):
+    for i in range(1, number + 1):
+        print(i)
+
+showNumber(5)
